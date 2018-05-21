0439022f0a16ae3da20502176d85fa5da657fcd1
omer257 <omer257@gmail.com>
2018-05-21 13:17:54 +0300

    changed port to work online

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ app.use(express.static('node_modules'))
 app.use(express.static('public'))
 
 
-app.listen(3005, () => console.log('Example app listening on port 3005!'))
+app.listen(process.env.PORT || 3005, () => console.log('Example app listening on port 3005!'))
