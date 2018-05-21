6e92749e0bd353635aad3dca02babbf00f1e72e2
omer257 <omer257@gmail.com>
2018-05-21 13:14:13 +0300

    make it better

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ app.use(express.static('node_modules'))
 app.use(express.static('public'))
 
 
-app.listen(3005, () => console.log('Example app listening on port 3000!'))
+app.listen(3005, () => console.log('Example app listening on port 3005!'))
